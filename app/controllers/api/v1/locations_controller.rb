e6f9939ecf8be93b89bb63d26a3bac073faa0242
yukitake <koshi.yukitake@gmail.com>
2023-08-22 15:28:37 +0900

    １ユーザ１投稿のため、投稿をUPSERTにする

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -24,7 +24,21 @@ class Api::V1::LocationsController < ApplicationController
 
     def create
         timestamp = Time.now.gmtime;
-        res = ActiveRecord::Base.connection.execute("INSERT INTO locations (uid,mid,point,thumbnail,preview_url,track_name,artist_name,created_at,updated_at) VALUES ('#{params[:uid]}', '#{params[:mid]}', ST_GeomFromText('SRID=4326;POINT(#{params[:lat]} #{params[:lng]})'), '#{params[:thumbnail]}', '#{params[:preview_url]}', '#{params[:track_name]}', '#{params[:artist_name]}', '#{timestamp}', '#{timestamp}')")
+        res = ActiveRecord::Base.connection.execute("
+            INSERT INTO locations 
+            (uid,mid,point,thumbnail,preview_url,track_name,artist_name,created_at,updated_at) VALUES 
+            ('#{params[:uid]}', '#{params[:mid]}', ST_GeomFromText('SRID=4326;POINT(#{params[:lat]} #{params[:lng]})'), '#{params[:thumbnail]}', '#{params[:preview_url]}', '#{params[:track_name]}', '#{params[:artist_name]}', '#{timestamp}', '#{timestamp}')
+            on conflict (uid) 
+            do update set 
+                mid = excluded.mid,
+                point = excluded.point,
+                thumbnail = excluded.thumbnail,
+                preview_url = excluded.preview_url,
+                track_name = excluded.track_name,
+                artist_name = excluded.artist_name,
+                created_at = excluded.created_at,
+                updated_at = excluded.updated_at
+        ")
 
         render json: { status: 200 }
     end
