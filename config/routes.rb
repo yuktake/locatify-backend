d1d114df4fcaeb846d545396faee5a59d52f54ad
yukitake <koshi.yukitake@gmail.com>
2023-08-31 15:18:40 +0900

    ヘルスチェック用のAPIを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get :health_check, to: 'health_check#index'
       resources :locations
       resources :users
     end
